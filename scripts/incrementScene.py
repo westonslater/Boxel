10c7e85218097d462c798581eaf7e6b6bbf560c0
= <=>
2024-01-09 13:36:46 -0800

    More initial commit stuff

---
+++ scripts/incrementScene.py
@@ -0,0 +1,88 @@
+import hou, sys, os, glob
+from os import listdir
+from os.path import isfile, join
+
+def getVersion(filename):
+    versionString = filename.split("_")[-1] if filename.split("_")[-1].startswith("v") else ""
+    version = versionString.split(".")[0]
+    if len(version) != 0:
+            version = int("".join(i for i in versionString if i.isdigit()))
+    else:
+            version = ""
+    return version,versionString    
+    
+def parseFileName(filename):
+    sceneFolder = hou.getenv("hip")  
+    version = getVersion(filename)
+    baseName = filename.rstrip(version[1]).rstrip("_")  
+    #baseName = filename.rstrip("filename.split("_")-1
+    print(f"""
+    Scene Folder: {sceneFolder}
+    Full Name: {filename}
+    Base Name: {baseName}
+    Version: {version[0]}
+    """)
+
+    if filename == "untitled":
+       hou.ui.displayMessage("Your scene is currently untitled")
+       sys.exit()     
+
+    if version[0] == "":
+        nov = hou.ui.displayMessage("No version detected. Add one?", buttons=('Yes','No'))
+        if nov == 0:            
+            saveScene(sceneFolder, baseName, version) 
+            sys.exit()
+        if nov == 1:
+            sys.exit()
+    return (filename, sceneFolder, baseName, version)    
+
+def findOtherVersions(sceneName):
+    versions = []
+    hipDir = hou.getenv('hip')
+    hipFiles = [f for f in glob.glob(hipDir + "*/*" + sceneName + "_*.hip")]
+    print(f"Found Versions in {hipDir}:")
+    for file in hipFiles:
+        ext = os.path.splitext(file)[1]
+        if ext == ".hip":
+            filename = os.path.split(file)[1]
+            print(" " + filename)
+            versions.append(filename)
+    return versions
+
+def saveScene(shotFolder, sceneName, version):
+    newHipname = sceneName+"_v"+format(int(version),'03d')
+    hou.hipFile.setName(shotFolder+"/"+newHipname+".hip")
+    hou.hipFile.save() 
+    print(f"Saved as {shotFolder}/{newHipname}.hip")
+ 
+def incrementVersion(version):
+    newVersion = int(version[0]) + 1
+    versionString = format(newVersion,'03d')
+    return versionString   
+    
+def __main__():            
+    hipname = hou.getenv("hipname")
+    fullName, sceneFolder, baseName, version = parseFileName(hipname)
+    versions = findOtherVersions(baseName)
+    newVersion = incrementVersion(version)
+    newName = f"{baseName}_v{newVersion}.hip"
+    
+    if newName in versions:
+        duplicateFound = hou.ui.displayMessage("Next version already exists.", buttons=('Save as Latest','Overwrite', 'Cancel'))
+        if duplicateFound == 0:
+            newMaxVersion = format(int(getVersion(versions[-1])[0]) + 1,'03d')
+            newName = f"{baseName}_v{newMaxVersion}.hip"
+            saveScene(sceneFolder, baseName, getVersion(newName)[0])
+            sys.exit()
+            
+        if duplicateFound == 1:
+            saveScene(sceneFolder, baseName, getVersion(newName)[0])
+            sys.exit()
+
+            
+    if newName not in versions:
+        saveScene(sceneFolder, baseName, getVersion(newName)[0])
+        sys.exit()
+
+
+__main__()
